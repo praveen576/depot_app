eeee49cc41180d2598023eb87c048fb192b8ddbd
praveen576 <sharma.praveen576@gmail.com>
2016-05-27 12:21:40 +0530

    Adding users

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
 
   has_many :line_items, dependent: :destroy
-  
+
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
 
   validates :name, :address, :email, presence: true
