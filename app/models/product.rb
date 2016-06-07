c1f50d4ce52b71d33293744820fcb75ae30664cc
praveen576 <sharma.praveen576@gmail.com>
2016-06-07 22:45:38 +0530

    adding default image for product and minor design fix

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,10 @@ class Product < ActiveRecord::Base
 
   has_attached_file :image, styles: { small: "100*100>" },
     url: "/assets/products/:id/:style/:basename.:extension",
-    path: ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+    path: ":rails_root/public/assets/products/:id/:style/:basename.:extension",
+    default_url: '/assets/products/default/missing.png'
+  
+
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 
